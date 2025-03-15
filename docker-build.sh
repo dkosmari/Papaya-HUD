203f9f7ad5352f0322207371cace0bcdee563695
Daniel K. O. (dkosmari) <none@none>
2025-03-15 00:45:47 -0300

    Use `-flto` in Docker builds for smaller executables.

--- docker-build.sh
+++ docker-build.sh
@@ -23,7 +23,7 @@ trap cleanup INT TERM
 docker build --tag $IMAGE . || cleanup 1
 
 ARGS="--tty --interactive --name $CONTAINER $IMAGE"
-docker run $ARGS sh -c "./bootstrap && ./configure --host=powerpc-eabi CXXFLAGS='-Os -ffunction-sections -fipa-pta' && make" || cleanup 2
+docker run $ARGS sh -c "./bootstrap && ./configure --host=powerpc-eabi CXXFLAGS='-Os -ffunction-sections -fipa-pta -Wno-odr -flto' AR=powerpc-eabi-gcc-ar RANLIB=powerpc-eabi-gcc-ranlib && make" || cleanup 2
 echo "Compilation finished."
 
 # Copy the wps file out.
